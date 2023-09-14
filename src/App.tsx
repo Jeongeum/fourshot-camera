aae893528a2027ce6fa6719719fcddd54ce362ec
Jeongeum <wjddma1202@naver.com>
2023-09-15 00:30:37 +0900

    style : 프레임 추가 및 스타일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 // src/App.tsx
-import React, { useState } from 'react';
-import WebcamCapture from './WebcamCapture';
-import Button from 'react-bootstrap/Button';
-import './App.css';
-import html2canvas from 'html2canvas'; // html2canvas 라이브러리 추가
+import React, { useState } from "react";
+import WebcamCapture from "./WebcamCapture";
+import Button from "react-bootstrap/Button";
+import "./App.css";
+import html2canvas from "html2canvas"; // html2canvas 라이브러리 추가
 
 function App() {
   const [capturedPhotos, setCapturedPhotos] = useState<string[]>([]);
@@ -23,14 +23,14 @@ function App() {
 
   const handleSaveImage = () => {
     if (capturedPhotos.length > 0) {
-      const frameContainer = document.querySelector('.wrapper') as HTMLElement;
+      const frameContainer = document.querySelector(".wrapper") as HTMLElement;
       if (frameContainer) {
         html2canvas(frameContainer).then((canvas) => {
           // canvas를 이미지로 변환하여 다운로드
-          const imageDataUrl = canvas.toDataURL('image/png');
-          const link = document.createElement('a');
+          const imageDataUrl = canvas.toDataURL("image/png");
+          const link = document.createElement("a");
           link.href = imageDataUrl;
-          link.download = 'captured_photos.png';
+          link.download = "captured_photos.png";
           link.click();
 
           // 이미지 저장 후 capturedPhotos 배열 초기화
@@ -46,7 +46,7 @@ function App() {
   return (
     <div className="App">
       <div className="wrapper">
-        <h1>인생네컷 포토이즘</h1>
+        <div className="frame-bg"></div>
         <div className="frame-container">
           {capturedPhotos.map((photo, index) => (
             <div key={index} className="frame">
@@ -57,7 +57,9 @@ function App() {
             <WebcamCapture onCapture={handleCapture} onSave={handleSaveImage} />
           ) : null}
           {capturedPhotos.length >= 3 && !showWebcamCapture ? (
-            <Button onClick={handleSaveImage}>이미지 저장</Button>
+            <Button className="photobtn" onClick={handleSaveImage}>
+              이미지 저장
+            </Button>
           ) : null}
         </div>
       </div>
